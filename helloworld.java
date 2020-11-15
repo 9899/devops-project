4bb6d99a59f7abe30be03ad1a0e70f68b4d715dc
9899 <unnat.1995@gmail.com>
2020-11-15 13:17:24 +0530

    added new print line

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,7 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");
+     System.out.println("welcome to terminal windos for the git");
      System.out.println("Hello Java1");
     }  
 }
